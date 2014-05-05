42a6a0f1e6d3f5142d094e13342d687e8648516c
Tiziano Lattisi <tiziano@axiastudio.it>
2014-05-05 21:52:09 +0200

    Parentize

--- src/com/axiastudio/zoefx/persistence/JPAManagerImpl.java
+++ src/com/axiastudio/zoefx/persistence/JPAManagerImpl.java
@@ -1,5 +1,6 @@
 package com.axiastudio.zoefx.persistence;
 
+import com.axiastudio.zoefx.core.beans.BeanAccess;
 import com.axiastudio.zoefx.core.db.Manager;
 
 import javax.persistence.EntityManager;
@@ -7,6 +8,12 @@ import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -39,6 +46,7 @@ public class JPAManagerImpl<E> implements Manager<E> {
 
     @Override
     public E commit(E entity) {
+        parentize(entity);
         EntityManager em = getEntityManager();
         em.getTransaction().begin();
         E merged = em.merge(entity);
@@ -80,4 +88,44 @@ public class JPAManagerImpl<E> implements Manager<E> {
     private EntityManager getEntityManager() {
         return entityManager;
     }
+
+    /*
+     * The parentize method hooks the items of the collections to the parent
+     * entity.
+     */
+    private void parentize(E entity){
+        for(Field f: entityClass.getDeclaredFields()){
+            for( Annotation a: f.getAnnotations()){
+                // discover the OneToMany
+                if( a.annotationType().equals(javax.persistence.OneToMany.class) ) {
+                    String name = f.getName();
+                    BeanAccess<Collection> collectionBeanAccess = new BeanAccess<Collection>(entity, name);
+                    Collection collection = collectionBeanAccess.getValue();
+                    if( collection != null && collection.size()>0 ){
+                        // discover the "mapped by" foreign key
+                        String foreignKey=null;
+                        try {
+                            Method mappedBy = a.annotationType().getDeclaredMethod("mappedBy");
+                            foreignKey = (String) mappedBy.invoke(a);
+                        } catch (NoSuchMethodException e) {
+                            e.printStackTrace();
+                        } catch (InvocationTargetException e) {
+                            e.printStackTrace();
+                        } catch (IllegalAccessException e) {
+                            e.printStackTrace();
+                        }
+                        if( foreignKey != null ) {
+                            // parentize children
+                            for (Iterator it = collection.iterator(); it.hasNext(); ) {
+                                Object child = it.next();
+                                BeanAccess<E> fkBeanAccess = new BeanAccess<>(child, foreignKey);
+                                fkBeanAccess.setValue(entity);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 }
